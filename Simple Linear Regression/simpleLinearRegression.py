e5e304dfd7ae8ef3d30bce32b7e8086b8371a52d
Hoang Le Doan <doanhoang1404@gmail.com>
2021-09-21 18:31:15 +0700

    add simple linear regression

---
+++ Simple Linear Regression/simpleLinearRegression.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#import dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+#splitting the dataset in to training set and testing set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state= 0)
+
+#trainig the simple linear regression model
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+#predict the test set values
+y_pred = regressor.predict(X_test)
+
+#visualizing the training set results

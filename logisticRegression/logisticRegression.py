c15006ee0cfe613d32da6342069565193110c0ca
Hoang Le Doan <83858447+hoangledoan@users.noreply.github.com>
2021-10-05 20:25:24 +0700

    Update logisticRegression.py

--- logisticRegression/logisticRegression.py
+++ logisticRegression/logisticRegression.py
@@ -29,3 +29,20 @@ np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)),
 from sklearn.metrics import confusion_matrix, accuracy_score
 print(confusion_matrix(y_test, y_pred))
 print(accuracy_score(y_test, y_pred))
+
+#plotting the logistic regression
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
